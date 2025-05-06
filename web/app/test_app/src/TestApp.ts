0b2057f5e49901f757a7fe3e2ad2043e8cc0a4b1
Jeff Ward <jeff.ward@gmail.com>
2025-05-06 14:58:30 -0600

    Upgraded to ESMBitFastDev - ver 18232b58

--- web/app/test_app/src/TestApp.ts
+++ web/app/test_app/src/TestApp.ts
@@ -1,12 +1,17 @@
 import { StopwatchTimerComponent } from "stopwatch_timer";
+import { JSUtil } from "jsutil";
 
 export class TestApp
 {
   stopwatchTimer: StopwatchTimerComponent;
 
-  constructor() {
+  constructor()
+  {
     const app_element = document.createElement('test-app');
     document.body.appendChild(app_element);
+    app_element.classList.add('esmbit-app');
+    const msg = JSUtil.calc_sha1(window.document.body.innerHTML);
+    app_element.innerHTML = `<div class="esmbit-app-header">My Test App - ${ msg }</div><div><img style="width:175px" src="assets://test_app/test_logo.svg"></div>`;
 
     // Initialize StopwatchTimer component
     this.stopwatchTimer = new StopwatchTimerComponent({
